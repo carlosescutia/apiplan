62dec6452bcbbebf3762883ebb17d8e8b94a38b8
Carlos Escutia <carlosescutia@gmail.com>
2022-07-27 14:14:03 -0500

    APG: Se completan ejes y lineas estratégicas.

--- application/models/Apg_model.php
+++ application/models/Apg_model.php
@@ -7,7 +7,17 @@ class Apg_model extends CI_Model {
 
     public function get_ejes() {
         $sql = ''
-            .'select '
+			.'select '
+			.'ide.contenido as id_eje, '
+			.'rnc.contenido as desc_eje  '
+			.'from '
+			.'"Registro_Nivel_Campo" rnc  '
+			.'left join "Registro_Nivel_Campo" ide on rnc.registro_nivel_id = ide.registro_nivel_id and ide.nivel_campo_id = 33 '
+			.'where  '
+			.'rnc.nivel_id = 8 '
+			.'and rnc.nivel_campo_id = 34 '
+			.'order by '
+			.'rnc.registro_nivel_id ; '
             .'';
         $query = $this->db->query($sql);
         return $query->result_array();
@@ -15,8 +25,21 @@ class Apg_model extends CI_Model {
 
     public function get_lineas() {
         $sql = ''
-            .'select '
-            .'';
+			.'select '
+			.'ide.contenido as id_eje, '
+			.'idl.contenido as id_linea, '
+			.'rnc.contenido as desc_linea  '
+			.'from '
+			.'"Registro_Nivel_Campo" rnc  '
+			.'left join "Registro_Nivel_Campo" idne on rnc.registro_nivel_id = idne.registro_nivel_id and idne.nivel_campo_id = 36 '
+			.'left join "Registro_Nivel_Campo" ide on idne.contenido::int = ide.registro_nivel_id and ide.nivel_id = 8 and ide.nivel_campo_id = 33 '
+			.'left join "Registro_Nivel_Campo" idl on rnc.registro_nivel_id = idl.registro_nivel_id and idl.nivel_campo_id = 37 '
+			.'where  '
+			.'rnc.nivel_id = 9 '
+			.'and rnc.nivel_campo_id = 38 '
+			.'order by '
+			.'rnc.registro_nivel_id ; '
+			.'';
         $query = $this->db->query($sql);
         return $query->result_array();
     }
