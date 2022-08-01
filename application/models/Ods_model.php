8dc4bf331a63c8588b791905463c3c0e2a0fec85
Carlos Escutia <carlosescutia@gmail.com>
2022-08-01 14:17:17 -0500

    ODS: Se completan objetivos y metas.

--- application/models/Ods_model.php
+++ application/models/Ods_model.php
@@ -7,7 +7,17 @@ class Ods_model extends CI_Model {
 
     public function get_objetivos() {
         $sql = ''
-            .'select '
+			.'select '
+			.'distinct on (rnc.registro_nivel_id) '
+			.'ido.contenido as id_objetivo, '
+			.'rnc.contenido as desc_objetivo '
+			.'from '
+			.'"Registro_Nivel_Campo" rnc  '
+			.'left join "Registro_Nivel_Campo" ido on rnc.registro_nivel_id = ido.registro_nivel_id and ido.nivel_campo_id = 105 '
+			.'where  '
+			.'rnc.nivel_id = 23 '
+			.'and rnc.nivel_campo_id = 106 '
+			.'order by rnc.registro_nivel_id, rnc.version desc; '
             .'';
         $query = $this->db->query($sql);
         return $query->result_array();
@@ -15,7 +25,20 @@ class Ods_model extends CI_Model {
 
     public function get_metas() {
         $sql = ''
-            .'select '
+			.'select '
+			.'distinct on (rnc.registro_nivel_id) '
+			.'ido.contenido as id_objetivo, '
+			.'idm.contenido as id_meta, '
+			.'left(rnc.contenido,60) as desc_meta '
+			.'from '
+			.'"Registro_Nivel_Campo" rnc  '
+			.'left join "Registro_Nivel_Campo" idno on rnc.registro_nivel_id = idno.registro_nivel_id and idno.nivel_campo_id = 192 '
+			.'left join "Registro_Nivel_Campo" ido on idno.contenido::int = ido.registro_nivel_id and ido.nivel_id = 23 and ido.nivel_campo_id = 105 '
+			.'left join "Registro_Nivel_Campo" idm on rnc.registro_nivel_id = idm.registro_nivel_id and idm.nivel_campo_id = 193 '
+			.'where  '
+			.'rnc.nivel_id = 33 '
+			.'and rnc.nivel_campo_id = 194 '
+			.'order by rnc.registro_nivel_id, rnc.version desc ; '
             .'';
         $query = $this->db->query($sql);
         return $query->result_array();
