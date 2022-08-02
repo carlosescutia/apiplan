f18912ed447a70ccba010f35a88f88c3abbe95ff
Carlos Escutia <carlosescutia@gmail.com>
2022-08-02 11:45:04 -0500

    PND: Se completan ejes y objetivos.

--- application/models/Pnd_model.php
+++ application/models/Pnd_model.php
@@ -5,17 +5,42 @@ class Pnd_model extends CI_Model {
         parent::__construct();
     }
 
-    public function get_objetivos() {
+    public function get_ejes() {
         $sql = ''
-            .'select '
+			.'select '
+			.'distinct on (rnc.registro_nivel_id) '
+			.'ide.contenido as id_eje, '
+			.'left(rnc.contenido,60) as desc_eje  '
+			.'from '
+			.'"Registro_Nivel_Campo" rnc  '
+			.'left join "Registro_Nivel_Campo" ide on rnc.registro_nivel_id = ide.registro_nivel_id and ide.nivel_campo_id = 214 '
+			.'where  '
+			.'rnc.nivel_id = 38 '
+			.'and rnc.nivel_campo_id = 215 '
+			.'order by '
+			.'rnc.registro_nivel_id, rnc.version desc '
             .'';
         $query = $this->db->query($sql);
         return $query->result_array();
     }
 
-    public function get_ejes() {
+    public function get_objetivos() {
         $sql = ''
             .'select '
+            .'distinct on (rnc.registro_nivel_id) '
+            .'ide.contenido as id_eje, '
+            .'ido.contenido as id_objetivo, '
+            .'rnc.contenido as desc_objetivo  '
+            .'from '
+            .'"Registro_Nivel_Campo" rnc  '
+            .'left join "Registro_Nivel_Campo" idne on rnc.registro_nivel_id = idne.registro_nivel_id and idne.nivel_campo_id = 217 '
+            .'left join "Registro_Nivel_Campo" ide on idne.contenido::int = ide.registro_nivel_id and ide.nivel_id = 38 and ide.nivel_campo_id = 214 '
+            .'left join "Registro_Nivel_Campo" ido on rnc.registro_nivel_id = ido.registro_nivel_id and ido.nivel_campo_id = 218 '
+            .'where  '
+            .'rnc.nivel_id = 39 '
+            .'and rnc.nivel_campo_id = 219 '
+            .'order by '
+            .'rnc.registro_nivel_id, rnc.version desc '
             .'';
         $query = $this->db->query($sql);
         return $query->result_array();
