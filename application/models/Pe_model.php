7281a845af0890eedca79228a7ca3fae7859f4c8
Carlos Escutia <carlosescutia@gmail.com>
2022-08-03 16:26:59 -0500

    PE: Se completan programas, líneas estratégicas, objetivos e indicadores.

--- application/models/Pe_model.php
+++ application/models/Pe_model.php
@@ -8,6 +8,17 @@ class Pe_model extends CI_Model {
     public function get_programas() {
         $sql = ''
             .'select '
+            .'distinct on (rnc.registro_nivel_id) '
+            .'idp.contenido as id_programa, '
+            .'rnc.contenido as desc_programa '
+            .'from '
+            .'"Registro_Nivel_Campo" rnc  '
+            .'left join "Registro_Nivel_Campo" idp on rnc.registro_nivel_id = idp.registro_nivel_id and idp.nivel_campo_id = 88 '
+            .'where  '
+            .'rnc.nivel_id = 19 '
+            .'and rnc.nivel_campo_id = 89 '
+            .'order by '
+            .'rnc.registro_nivel_id, rnc.version desc '
             .'';
         $query = $this->db->query($sql);
         return $query->result_array();
@@ -16,6 +27,20 @@ class Pe_model extends CI_Model {
     public function get_lineas() {
         $sql = ''
             .'select '
+            .'distinct on (rnc.registro_nivel_id) '
+            .'idp.contenido as id_programa, '
+            .'idl.contenido as id_linea, '
+            .'rnc.contenido as desc_linea '
+            .'from '
+            .'"Registro_Nivel_Campo" rnc  '
+            .'left join "Registro_Nivel_Campo" idnp on rnc.registro_nivel_id = idnp.registro_nivel_id and idnp.nivel_campo_id = 91 '
+            .'left join "Registro_Nivel_Campo" idp on idnp.contenido::int = idp.registro_nivel_id and idp.nivel_id = 19 and idp.nivel_campo_id = 88 '
+            .'left join "Registro_Nivel_Campo" idl on rnc.registro_nivel_id = idl.registro_nivel_id and idl.nivel_campo_id = 92 '
+            .'where  '
+            .'rnc.nivel_id = 20 '
+            .'and rnc.nivel_campo_id = 93 '
+            .'order by '
+            .'rnc.registro_nivel_id, rnc.version desc '
             .'';
         $query = $this->db->query($sql);
         return $query->result_array();
@@ -24,6 +49,23 @@ class Pe_model extends CI_Model {
     public function get_objetivos() {
         $sql = ''
             .'select '
+            .'distinct on (rnc.registro_nivel_id) '
+            .'idp.contenido as id_programa, '
+            .'idl.contenido as id_linea, '
+            .'ido.contenido as id_objetivo, '
+            .'rnc.contenido as desc_objetivo '
+            .'from '
+            .'"Registro_Nivel_Campo" rnc  '
+            .'left join "Registro_Nivel_Campo" idnp on rnc.registro_nivel_id = idnp.registro_nivel_id and idnp.nivel_campo_id = 95 '
+            .'left join "Registro_Nivel_Campo" idnl on rnc.registro_nivel_id = idnl.registro_nivel_id and idnl.nivel_campo_id = 96 '
+            .'left join "Registro_Nivel_Campo" idp on idnp.contenido::int = idp.registro_nivel_id and idp.nivel_id = 19 and idp.nivel_campo_id = 88 '
+            .'left join "Registro_Nivel_Campo" idl on idnl.contenido::int = idl.registro_nivel_id and idl.nivel_id = 20 and idl.nivel_campo_id = 92 '
+            .'left join "Registro_Nivel_Campo" ido on rnc.registro_nivel_id = ido.registro_nivel_id and ido.nivel_campo_id = 97 '
+            .'where  '
+            .'rnc.nivel_id = 21 '
+            .'and rnc.nivel_campo_id = 98 '
+            .'order by '
+            .'rnc.registro_nivel_id, rnc.version desc '
             .'';
         $query = $this->db->query($sql);
         return $query->result_array();
@@ -32,6 +74,26 @@ class Pe_model extends CI_Model {
     public function get_indicadores() {
         $sql = ''
             .'select '
+            .'distinct on (rnc.registro_nivel_id) '
+            .'idp.contenido as id_programa, '
+            .'idl.contenido as id_linea, '
+            .'ido.contenido as id_objetivo, '
+            .'idi.contenido as id_indicador, '
+            .'rnc.contenido as desc_indicador '
+            .'from '
+            .'"Registro_Nivel_Campo" rnc  '
+            .'left join "Registro_Nivel_Campo" idnp on rnc.registro_nivel_id = idnp.registro_nivel_id and idnp.nivel_campo_id = 100 '
+            .'left join "Registro_Nivel_Campo" idnl on rnc.registro_nivel_id = idnl.registro_nivel_id and idnl.nivel_campo_id = 101 '
+            .'left join "Registro_Nivel_Campo" idno on rnc.registro_nivel_id = idno.registro_nivel_id and idno.nivel_campo_id = 102 '
+            .'left join "Registro_Nivel_Campo" idp on idnp.contenido::int = idp.registro_nivel_id and idp.nivel_id = 19 and idp.nivel_campo_id = 88 '
+            .'left join "Registro_Nivel_Campo" idl on idnl.contenido::int = idl.registro_nivel_id and idl.nivel_id = 20 and idl.nivel_campo_id = 92 '
+            .'left join "Registro_Nivel_Campo" ido on idno.contenido::int = ido.registro_nivel_id and ido.nivel_id = 21 and ido.nivel_campo_id = 97 '
+            .'left join "Registro_Nivel_Campo" idi on rnc.registro_nivel_id = idi.registro_nivel_id and idi.nivel_campo_id = 103 '
+            .'where  '
+            .'rnc.nivel_id = 22 '
+            .'and rnc.nivel_campo_id = 104 '
+            .'order by '
+            .'rnc.registro_nivel_id, rnc.version desc '
             .'';
         $query = $this->db->query($sql);
         return $query->result_array();
